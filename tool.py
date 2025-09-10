81499564408287bfbccdc04b4eaaa3723e209309
WalidMaalej <64880625+WalidMaalej@users.noreply.github.com>
2025-09-10 07:02:12 +0100

    v0.2

--- tool.py
+++ tool.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from bs4 import BeautifulSoup, NavigableString, Tag
